4c2ab520938d95650cbdcf412a28336eaae828e9
Sanjanashalini K <sanjanashalini64768@gmail.com>
2024-01-02 12:52:31 +0530

    Day-7

---
+++ Day-7/myapp/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./assests/css/sample.css"
+import Sample from './components/sample'
+function App(){
+  return(
+    <div>
+         <Sample/>
+  </div>
+  )
+}
+export default App
